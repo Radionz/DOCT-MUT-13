c5dd7ab713ac022412fbe9f4603b7a4f4128e006
Julien Andre <julienandre1199@gmail.com>
2016-02-23 14:36:17 +0100

    [Fixed] package changed

--- src/main/java/fr/unice/polytech/doct13/plugin/CodeGeneratorMojo.java
+++ src/main/java/fr/unice/polytech/doct13/plugin/CodeGeneratorMojo.java
@@ -1,14 +1,14 @@
-package com.excilys.labs.maven.plugin.generator;
+package fr.unice.polytech.doct13.plugin;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
 /**
- * A goal to generate code.
+ * A goal to mutate code.
  *
- * @goal generate
- * @phase generate-sources
+ * @goal mutate
+ * @phase mutate-sources
  */
 public class CodeGeneratorMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException, MojoFailureException {
