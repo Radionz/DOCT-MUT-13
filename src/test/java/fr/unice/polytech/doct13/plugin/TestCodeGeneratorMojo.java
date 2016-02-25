a120998b799f03d90c203504f6b3b6d4fc8202a1
Dorian Blanc <Dorian Blanc>
2016-02-25 12:29:25 +0100

    refactor en vue de la crétaion d'un autre repo et projet pour le programme qui subira les mutations

--- src/test/java/fr/unice/polytech/doct13/plugin/TestCodeGeneratorMojo.java
+++ src/test/java/fr/unice/polytech/doct13/plugin/TestCodeGeneratorMojo.java
@@ -4,14 +4,6 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
-/**
- * A goal to generate code.
- *
- * @goal generate
- * @phase generate-sources
- */
-public class TestCodeGeneratorMojo extends AbstractMojo {
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Hello World!");
-    }
+public class TestCodeGeneratorMojo {
+
 }
