b5cbdafc413f6e6d98c93d52d02d2596d87b36f6
Dorian Blanc <Dorian Blanc>
2016-02-25 23:52:38 +0100

    amélioration rapport html

--- src/main/java/fr/unice/polytech/doct13/plugin/report/HTMLhelper.java
+++ src/main/java/fr/unice/polytech/doct13/plugin/report/HTMLhelper.java
@@ -1,14 +1,19 @@
 package fr.unice.polytech.doct13.plugin.report;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by blanc on 25/02/2016.
  */
 public class HTMLhelper {
 
-    public static String inHTMLbody(String inside){
-        String html = "";
+    public static String inHTMLbody(String insideHead, String insideBody) {
+        String html = "<!DOCTYPE HTML>";
         html += "<html>\n";
         html += "<head>\n";
+        html += "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>\n";
+        html += "<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js'></script>\n";
         html += "<title>Mutation Report</title>\n";
         html += "<style>\n";
         html += "#tableMutants{border:1px solid;margin:0 auto;}" +
@@ -16,12 +21,62 @@ public class HTMLhelper {
                 ".aliveMut{background-color:red;}" +
                 ".deadMut{background-color:green;}\n";
         html += "</style>\n";
+        html += insideHead;
         html += "</head>\n";
         html += "<body>\n";
-        html += inside;
+        html += insideBody;
         html += "</body>\n";
         html += "</html>\n";
         return html;
     }
+
+    public static String highchartsPie(String title, int aliveMutant, int deadMutant) {
+
+        String html = "<script type='text/javascript'>" +
+                "                $(function () {" +
+                "            $('#container').highcharts({" +
+                "                    chart: {" +
+                "                plotBackgroundColor: null," +
+                "                        plotBorderWidth: null," +
+                "                        plotShadow: false," +
+                "                        type: 'pie'" +
+                "            }," +
+                "            title: {" +
+                "                text: '" + title + "'" +
+                "            }," +
+                "            tooltip: {" +
+                "                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'" +
+                "            }," +
+                "            plotOptions: {" +
+                "                pie: {" +
+                "                    allowPointSelect: true," +
+                "                            cursor: 'pointer'," +
+                "                            dataLabels: {" +
+                "                        enabled: true," +
+                "                                format: '<b>{point.name}</b>: {point.percentage:.1f} %'," +
+                "                                style: {" +
+                "                                   color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'" +
+                "                        }" +
+                "                    }" +
+                "                }" +
+                "            }," +
+                "            series: [{" +
+                "                name: 'Percentage'," +
+                "                        colorByPoint: true," +
+                "                        data: [{" +
+                "                    name: 'Alive'," +
+                "                            y: " + aliveMutant + "}, {" +
+                "                    name: 'Dead'," +
+                "                            y: " + deadMutant + "}]" +
+                "            }]" +
+                "            });" +
+                "        });" +
+                "        </script>" +
+                "        </head>" +
+                "        <body>" +
+                "        <script src='https://code.highcharts.com/highcharts.js'></script>";
+        return html;
+    }
+
 
 }
--- src/main/java/fr/unice/polytech/doct13/plugin/report/ReportMojo.java
+++ src/main/java/fr/unice/polytech/doct13/plugin/report/ReportMojo.java
@@ -1,4 +1,5 @@
 package fr.unice.polytech.doct13.plugin.report;
+
 import fr.unice.polytech.doct13.plugin.report.HTMLhelper;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.maven.plugin.AbstractMojo;
@@ -19,6 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -26,20 +28,20 @@ import java.util.List;
  * Created by blanc on 25/02/2016.
  */
 
-@Mojo(name="report")
-public class ReportMojo extends AbstractMojo{
+@Mojo(name = "report")
+public class ReportMojo extends AbstractMojo {
 
-    @Parameter(defaultValue = "${project}", required=true,readonly = true)
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
     private MavenProject project;
 
-    public void execute() throws MojoExecutionException,MojoFailureException{
+    public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info("HTML Report");
-        System.out.println(project.getBasedir()+"/target/surefire-reports");
+        System.out.println(project.getBasedir() + "/target/surefire-reports");
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = null;
 
-        File dirTarget = new File(project.getBasedir()+"/target/html-report");
-        if(!dirTarget.exists()){
+        File dirTarget = new File(project.getBasedir() + "/target/html-report");
+        if (!dirTarget.exists()) {
             dirTarget.mkdir();
         }
 
@@ -62,41 +64,43 @@ public class ReportMojo extends AbstractMojo{
             }
             PrintWriter writer = new PrintWriter(htmlReport.getAbsolutePath(), "UTF-8");
 
-
-            int mutantVivant = 0;
-            int mutantMort = 0;
+            int aliveMutant = 0;
+            int deadMutant = 0;
             String html = "";
-            for (NodeList nl : nlList){
+            for (NodeList nl : nlList) {
                 String className = "";
                 for (int i = 0; i < nl.getLength(); i++) {
                     Element elem = (Element) nl.item(i);
                     if (className.isEmpty()) {
                         className = elem.getAttribute("classname");
-                        html += "<div style='border: 1px solid;background-color: #EEE'>Dans la classe : " + className + "</div>\n";
-                        html += "<table id='tableMutants'><tr><td>Mutant Vivant</td><td>Mutant Tue</td></tr>\n";
+                        html += "<div style='border: 1px solid;background-color: #EEE'>Class : " + className + "</div>\n";
+                        html += "<table id='tableMutants'><tr><td>Alive (Method)</td><td>Dead (Method)</td></tr>\n";
                     }
                     if (elem.getChildNodes().getLength() == 0) {
-                        html += "<tr><td class='aliveMut'>Dans la methode " + elem.getAttribute("name") + "</td><td></td></tr>\n";
-                        mutantVivant++;
+                        html += "<tr><td class='aliveMut'>" + elem.getAttribute("name") + "</td><td></td></tr>\n";
+                        aliveMutant++;
                     } else {
-                        html += "<tr><td></td><td class='deadMut'>Dans la methode " + elem.getAttribute("name") + "</td></tr>\n";
-                        mutantMort++;
+                        html += "<tr><td></td><td class='deadMut'>" + elem.getAttribute("name") + "</td></tr>\n";
+                        deadMutant++;
                     }
                 }
                 html += "</table>\n";
-                html += "<div style='text-align: center;'>Nombre de Mutants vivant : "+mutantVivant+"</div>\n";
-                html += "<div style='text-align: center;'>Nombre de Mutants mort : "+mutantMort+"</div>\n";
-                float porcentageDeadMut = ((float)mutantMort/((float)mutantMort+(float)mutantVivant))*100;
-                float porcentageAliveMut = (((float)mutantVivant/((float)mutantMort+(float)mutantVivant))*100);
-                html += "<div style='text-align: center;'>% de Mutants mort : "+porcentageDeadMut+"% </div>\n";
-                html += "<div style='text-align: center;'>% de Mutants vivant : "+porcentageAliveMut+"% </div>\n";
+                html += "<div style='text-align: center;'>Alive : " + aliveMutant + "</div>\n";
+                html += "<div style='text-align: center;'>Dead : " + deadMutant + "</div>\n";
+                float porcentageDeadMut = ((float) deadMutant / ((float) deadMutant + (float) aliveMutant)) * 100;
+                float porcentageAliveMut = (((float) aliveMutant / ((float) deadMutant + (float) aliveMutant)) * 100);
             }
-            writer.print(HTMLhelper.inHTMLbody(html));
+            html += "<br/><div id=\'container\' style=\'min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto\'></div>\n";
+            writer.print(HTMLhelper.inHTMLbody(HTMLhelper.highchartsPie("Chart Alive / Dead", aliveMutant, deadMutant), html));
+
 
             writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
         }
-        catch (IOException e) {e.printStackTrace();}
-        catch (ParserConfigurationException e) {e.printStackTrace();}
-        catch (SAXException e) {e.printStackTrace();}
     }
 }
